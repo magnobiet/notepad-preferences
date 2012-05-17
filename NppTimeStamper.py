950dc858a12f7ccfda514ce20a2c35494fdbded1
Magno F. B. Biét <magno.biet@gmail.com>
2012-05-17 09:51:29 -0300

    Include "HTMLTag-entities.ini", "NppTimeStamper.py"

---
+++ NppTimeStamper.py
@@ -0,0 +1,36 @@
+﻿import datetime
+import re
+import math
+
+def timestamper(args):
+      editor.setUndoCollection(False)
+
+      currentLang = str(notepad.getCurrentLang())
+
+      if re.search('(JS|CSS)$', currentLang) :
+            timestampPattern = '\d{6,8}-\d{2,4}'
+            timestampAlreadyExisted = False
+            firstVisibleLine = editor.getFirstVisibleLine()
+            currentPosition = editor.getCurrentPos()
+            currentLine = editor.lineFromPosition(currentPosition)
+            newTimestamp = datetime.datetime.now().strftime('%Y%m%d-%H%M')
+            limit = min(30, editor.getLineCount())
+
+            for i in range(0, limit):
+
+                  if re.search(timestampPattern, editor.getLine(i)) :
+                        text = re.sub(timestampPattern, newTimestamp, editor.getLine(i))
+                        editor.replaceWholeLine(i, text)
+                        timestampAlreadyExisted = True
+
+            if timestampAlreadyExisted == False:
+                  newTimestampComment = '/*! {0} */'.format(newTimestamp) + '\n\n'
+                  editor.insertText(0, newTimestampComment)
+                  currentPosition = currentPosition + len(newTimestampComment)
+
+            notepad.menuCommand(MENUCOMMAND.FORMAT_CONV2_UTF_8)
+            editor.gotoPos(currentPosition)
+
+      editor.setUndoCollection(True)
+
+notepad.callback(timestamper, [NOTIFICATION.FILEBEFORESAVE])
